423e016d024cf4d8e19ef6a2698cd19506764e2a
jake-ritz11 <61728158+jake-ritz11@users.noreply.github.com>
2022-04-11 23:49:42 -0600

    2

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from './heroes/hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Batman' },
+    { id: 12, name: 'Superman' },
+    { id: 13, name: 'Wonder Woman' },
+    { id: 14, name: 'Flash' },
+    { id: 15, name: 'Aquaman' },
+    { id: 16, name: 'Cyborg' },
+    { id: 17, name: 'Martian' },
+    { id: 18, name: 'Green Lantern' },
+    { id: 19, name: 'Green Arrow' },
+    { id: 20, name: 'Hawkgirl' },
+]
