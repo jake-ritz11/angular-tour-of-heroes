32b25023ad94658a577eed4b5bb5b7229ba69947
jake-ritz11 <61728158+jake-ritz11@users.noreply.github.com>
2022-04-12 09:32:52 -0600

    5

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-
+import { Observable, of } from 'rxjs';
 import {Hero} from './heroes/hero';
 import { HEROES } from './mock-heroes';
 
@@ -8,8 +8,9 @@ import { HEROES } from './mock-heroes';
 })
 export class HeroService {
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    return heroes;
   }
 
   constructor() { }
